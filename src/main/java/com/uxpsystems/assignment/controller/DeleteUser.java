374dbd166cfe183689d4d6066af91448a8f51b9f
mkprasanth <41589481+mkprasanth@users.noreply.github.com>
2019-03-29 00:42:05 -0500

    Update DeleteUser.java

--- src/main/java/com/uxpsystems/assignment/controller/DeleteUser.java
+++ src/main/java/com/uxpsystems/assignment/controller/DeleteUser.java
@@ -1,10 +1,12 @@
 package com.uxpsystems.assignment.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.uxpsystems.assignment.dao.User;
 import com.uxpsystems.assignment.service.UserRepository;
 
 @RestController
@@ -18,7 +20,15 @@ public class DeleteUser {
 	}	
 	
 	@DeleteMapping(path="/users/id/{id}")
-	public void deleteUsersById(@PathVariable long id) {
-				userRepository.deleteById(id);
+	public void deleteUsersById(@PathVariable long id) {				
+
+		Optional<User> existingUser = userRepository.findById(id);
+
+		if (existingUser.isPresent()) {
+			userRepository.deleteById(id);
+		} else {
+			throw new UsernameNotFoundException("User doesnt exist in the system. Please check the userID");
+		}
+	
 	}
 }
